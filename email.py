783b16736d8875af8399327d5fd2d4f74b558572
Prateek Thakur <prateekthakur272@gmail.com>
2024-01-01 10:57:01 +0530

    delete env

--- email.py
+++ email.py
@@ -1,1 +1,15 @@
 from fastapi import (BackgroundTasks, UploadFile, File, Form, Depends, HTTPException, status)
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from dotenv import load_dotenv
+
+
+consfig = ConnectionConfig(
+    MAIL_USERNAME = '',
+    MAIL_PASSWORD = '',
+    MAIL_FROM = '',
+    MAIL_PORT = 587,
+    MAIL_SERVER= 'smtp.google.com',
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True
+)
+
